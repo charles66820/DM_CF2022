d124536e1d88161af5210afb770dbd31a901e8cb
Amandine_Semeteys <amandine.semeteys@etu.u-bordeaux.fr>
2022-04-27 18:59:24 +0200

    fin exo1

--- code/swap-xor.h
+++ code/swap-xor.h
@@ -1,4 +1,9 @@
-/*@ terminates \true;
+/*@ requires mem_access: \valid_read(a);
+    requires mem_access : \valid_read(b);
+    requires \valid(a);
+    requires \valid(b);
+    requires \separated(a,b);
+    terminates \true;
     ensures *a == \old(*b);
     ensures *b == \old(*a);
 */
