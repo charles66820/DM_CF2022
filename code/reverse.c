a7a61d74b24e5762d398d6b65914b52231db6e06
charles66820 <charles.goedefroit@gmail.com>
2022-04-29 15:28:01 +0200

    begin frama-c

--- code/reverse.c
+++ code/reverse.c
@@ -2,6 +2,15 @@
 
 void reverse(int *t, unsigned int i, unsigned int j, unsigned int n)
 {
+    /*@
+      loop invariant i < j + 1;
+      loop invariant 0 <= i <= n;
+      loop invariant n >= j >= 0;
+      loop invariant t[i] == \at(t[j], Pre);
+      loop invariant t[j] == \at(t[i], Pre);
+      // loop invariant \forall integer k; 0 <= i <= k <= j <= n ==> \valid(t + k);
+      loop assigns i, j, t;
+    */
     while (i < j)
     {
         int aux = t[i];
--- code/reverse.h
+++ code/reverse.h
@@ -1,4 +1,11 @@
+#include "limits.h"
+
 /*@
+    requires \forall integer k; 0 <= i <= k <= j < n ==> \valid(t + k);
+    requires \forall integer k; 0 <= i <= k <= j < n ==> \valid_read(t + k);
+    requires 0 < n <= INT_MAX;
+    requires 0 <= i <= n;
+    requires n >= j >= 0;
     terminates \true;
     ensures \forall integer k; i <= k <= j ==> t[k] == \old(t[j-k+i]);
 */
