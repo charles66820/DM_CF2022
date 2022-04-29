b4d393f43293c5d646075eeb2931265ab02b5873
Amandine_Semeteys <amandine.semeteys@etu.u-bordeaux.fr>
2022-04-29 17:16:56 +0200

    vous êtes des génies !

--- code/reverse.c
+++ code/reverse.c
@@ -3,13 +3,15 @@
 void reverse(int *t, unsigned int i, unsigned int j, unsigned int n)
 {
     /*@
-      loop invariant i < j + 1;
-      loop invariant 0 <= i <= n;
-      loop invariant n >= j >= 0;
-      loop invariant t[i] == \at(t[j], Pre);
-      loop invariant t[j] == \at(t[i], Pre);
-      // loop invariant \forall integer k; 0 <= i <= k <= j <= n ==> \valid(t + k);
-      loop assigns i, j, t;
+      loop invariant i <= j+1;
+      loop invariant i+j == (\at(i,Pre)) + (\at(j,Pre));
+      loop invariant i >= \at(i,Pre);
+      loop invariant j <= \at(j,Pre);
+      loop invariant \forall integer k; (\at(i,Pre)) <= k < i ==> t[k] == \at(t[(\at(j,Pre))-k+(\at(i,Pre))],Pre);
+      loop invariant \forall integer k; j < k <= (\at(j,Pre)) ==> t[k] == \at(t[(\at(j,Pre))-k+(\at(i,Pre))],Pre);
+      loop invariant \forall integer k; i <= k <= j ==> t[k] == \at(t[k],Pre);
+      loop assigns i, j, t[\at(i,Pre) .. \at(j,Pre)];
+      loop variant j;
     */
     while (i < j)
     {
--- code/reverse.h
+++ code/reverse.h
@@ -1,12 +1,12 @@
 #include "limits.h"
 
 /*@
-    requires \forall integer k; 0 <= i <= k <= j < n ==> \valid(t + k);
-    requires \forall integer k; 0 <= i <= k <= j < n ==> \valid_read(t + k);
+    requires \valid(t + (0 .. n-1));
+    requires \valid_read(t + (0 .. n-1));
     requires 0 < n <= INT_MAX;
-    requires 0 <= i <= n;
-    requires n >= j >= 0;
+    requires 0 <= i < j < n;
     terminates \true;
+    assigns t[i .. j];
     ensures \forall integer k; i <= k <= j ==> t[k] == \old(t[j-k+i]);
 */
 void reverse(int *t, unsigned int i, unsigned int j, unsigned int n);
