d28a72ff88bb276ff51ad2a4f5e3135bbfbf6172
charles66820 <charles.goedefroit@gmail.com>
2022-04-29 19:01:13 +0200

    rec fini

--- code/reverse.h
+++ code/reverse.h
@@ -15,13 +15,11 @@ void reverse(int *t, unsigned int i, unsigned int j, unsigned int n);
     requires \valid(t + (0 .. n-1));
     requires \valid_read(t + (0 .. n-1));
     requires 0 < n <= INT_MAX;
-    requires 0 <= i < j < n;
+    requires 0 <= i < n;
+    requires 0 <= j < n;
     terminates \true;
     decreases j;
     assigns t[i .. j];
     ensures \forall integer k; i <= k <= j ==> t[k] == \old(t[j-k+i]);
-    ensures 0 <= i + 1 < j - 1 < n;
-    ensures i > \old(i);
-    ensures j < \old(j);
 */
 void revers_rec(int *t, unsigned int i, unsigned int j, unsigned int n);
