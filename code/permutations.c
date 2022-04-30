313b8ba951891b1578dd4406f2e73d7206141b25
Quentin Désiré <qtin97490@outlook.fr>
2022-04-30 22:33:23 +0200

    sé vèr

--- code/permutations.c
+++ code/permutations.c
@@ -2,6 +2,15 @@
 
 void initPerm(int *P, unsigned int n) {
   int i = 0;
+  /*@
+    loop invariant 0 <= i <= n;
+    //loop invariant \forall integer k; 0 <= k < i ==> P[k] < P[k+1];
+    loop invariant unchangedTab{Pre,Here}(P,P,i,n);
+    loop invariant isPermutation(P,i);
+    loop invariant isMinPerm(P,i);
+    loop assigns i, P[0 .. n-1];
+    loop variant n-i;
+  */
   while (i < n)
   {
     P[i] = i;
--- code/permutations.h
+++ code/permutations.h
@@ -2,13 +2,17 @@
 #include "limits.h"
 
 /*@
+    requires \valid(P + (0 .. n-1));
+    requires 0 < n < INT_MAX;
     terminates \true;
+    assigns P[0 .. n-1];
     ensures isPermutation(P,n);
     ensures isMinPerm(P,n);
 */
 void initPerm(int *P, unsigned int n);
 
 /*@
+    requires \valid_read(P + (0 .. n-1));
     terminates \true;
     ensures isMaxPerm(P,n) ==> \result == true;
     ensures !isMaxPerm(P,n) ==> \result == false;
