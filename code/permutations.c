7309178c8dccb2839e50f279864e0c9a558641d1
charles66820 <charles.goedefroit@gmail.com>
2022-04-30 19:34:06 +0200

    code `initPerm`

--- code/permutations.c
+++ code/permutations.c
@@ -1,13 +1,16 @@
 #include "permutations.h"
 
-void initPerm(int *P, unsigned int n)
-{
-    // TODO
-    return;
+void initPerm(int *P, unsigned int n) {
+  int i = 0;
+  while (i < n)
+  {
+    P[i] = i;
+  }
+
+  return;
 }
 
-bool isMaxPerm(int *P, unsigned int n)
-{
-    // TODO
-    return true;
+bool isMaxPerm(int *P, unsigned int n) {
+  // TODO
+  return true;
 }
