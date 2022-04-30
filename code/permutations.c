f4758cf0730821cb454c8d705e5fd9c7dac5172b
charles66820 <charles.goedefroit@gmail.com>
2022-05-01 00:03:00 +0200

    ça fonctionne pas

--- code/permutations.c
+++ code/permutations.c
@@ -24,9 +24,19 @@ bool isMaxPerm(int *P, unsigned int n) {
   bool res = true;
 
   int i = n - 1;
+  /*@
+    loop invariant -1 <= i < n;
+    loop invariant 0 == res || res == 1;
+    //loop invariant \forall integer k; 0 <= k < i ==> P[k] < P[k+1];
+    loop invariant unchangedTab{Pre,Here}(P,P,0,n);
+    loop invariant isPermutation(P,n-1);
+    loop invariant isMaxPerm(P,i);
+    loop assigns i, P[0 .. n-1], res;
+    loop variant i;
+  */
   while (i >= 0)
   {
-    if (P[i] == i) {
+    if (P[n-1-i] == i) {
       res = false;
       break;
     }
